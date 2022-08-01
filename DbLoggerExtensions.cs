431740ea5275fc25cc8205a94c1eb0e6fd353059
SaiUpendram <110382170+SaiUpendram@users.noreply.github.com>
2022-08-01 14:14:45 +0530

    Create DbLoggerExtensions.cs

---
+++ DbLoggerExtensions.cs
@@ -0,0 +1,9 @@
+public static class DbLoggerExtensions
+{
+    public static ILoggingBuilder AddDbLogger(this ILoggingBuilder builder, Action<DbLoggerOptions> configure)
+    {          
+        builder.Services.AddSingleton<ILoggerProvider, DbLoggerProvider>();
+        builder.Services.Configure(configure);
+        return builder;
+    }
+}

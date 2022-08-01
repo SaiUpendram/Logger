9eee851326403fdc5cb66d8782dd6e0d4c5e4984
SaiUpendram <110382170+SaiUpendram@users.noreply.github.com>
2022-08-01 13:56:59 +0530

    Create DbLoggerOptions.cs

---
+++ DbLoggerOptions.cs
@@ -0,0 +1,13 @@
+ 
+public class DbLoggerOptions
+{
+    public string ConnectionString { get; init; }
+ 
+    public string[] LogFields { get; init; }
+ 
+    public string LogTable { get; init; }
+ 
+    public DbLoggerOptions()
+    {
+    }
+}

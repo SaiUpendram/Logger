608d825730afe7633fa21dae45fc1b9f5989834a
SaiUpendram <110382170+SaiUpendram@users.noreply.github.com>
2022-08-01 14:05:47 +0530

    Create DbLoggerProvider.cs

---
+++ DbLoggerProvider.cs
@@ -0,0 +1,18 @@
+public class DbLoggerProvider : ILoggerProvider
+{
+    public readonly DbLoggerOptions Options;
+ 
+    public DbLoggerProvider(IOptions<DbLoggerOptions> _options)
+    {
+        Options = _options.Value; 
+    }
+ 
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new DbLogger(this);
+    }
+ 
+    public void Dispose()
+    {
+    }
+}
